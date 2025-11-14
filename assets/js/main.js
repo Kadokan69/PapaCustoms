fd6d0626ed53c7d05227497324625d8d4accf1a1
Andrey Kelorev <kelorev.andrey@yandex.ru>
2025-11-14 13:31:45 +0300

    feat: mask phone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 import { mainJs } from './index.js';
 import { Modal } from './modal.js';
 import { ModalVideo } from './modalVideo.js';
+import { Mask } from './mask.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   mainJs();
   Modal();
   ModalVideo();
+  Mask();
 });
